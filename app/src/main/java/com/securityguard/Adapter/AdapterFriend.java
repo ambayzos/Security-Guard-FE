255bb7af1eae7eaf13c189c1e06a1bdd3da164db
ambayzos <ahmadhambali1997@gmail.com>
2021-10-22 17:29:46 +0700

    change listView dengan ReyclerView

--- app/src/main/java/com/securityguard/Adapter/AdapterFriend.java
+++ app/src/main/java/com/securityguard/Adapter/AdapterFriend.java
@@ -1,27 +1,47 @@
 package com.securityguard.Adapter;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
-public class AdapterFriend extends BaseAdapter {
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.securityguard.R;
+import com.securityguard.entity.Friend;
+import com.securityguard.entity.UserEntity;
+
+import java.util.ArrayList;
+
+public class AdapterFriend extends RecyclerView.Adapter<AdapterFriend.ViewHolder> {
+
+    private ArrayList<UserEntity> listViewFriends;
+    @NonNull
     @Override
-    public int getCount() {
-        return 0;
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_list_friends, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
-    public Object getItem(int position) {
-        return null;
+    public void onBindViewHolder(@NonNull AdapterFriend.ViewHolder viewHolder, final int position) {
+        viewHolder.txtNama.setText(listViewFriends.get(position).getNama());
+        viewHolder.txtNoHp.setText(listViewFriends.get(position).getNoTelp());
     }
 
     @Override
-    public long getItemId(int position) {
-        return 0;
+    public int getItemCount() {
+        return listViewFriends.size();
     }
 
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private final TextView txtNama, txtNoHp;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            txtNama = itemView.findViewById(R.id.tx_Nama);
+            txtNoHp = itemView.findViewById(R.id.tx_NoHp);
+        }
     }
 }
